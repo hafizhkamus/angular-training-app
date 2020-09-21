654248c2f2bd418dad5b61b0db77fafbfb07a7fc
hafizhkamus <hafizhkamus13@gmail.com>
2020-09-21 11:58:44 +0700

    friends component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FriendsComponent } from './friends/friends.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
